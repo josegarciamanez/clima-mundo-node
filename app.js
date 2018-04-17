84212f0d599155aaabc09f7dcf3b60b631019b32
Jose Garcia <josegarciamanez@gmail.com>
2018-04-17 23:37:05 +0200

    obtener direccion, lat y lng con axios

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
+const axios = require('axios')
 const argv = require('yargs').options({
     direccion: {
         alias: 'd',
         desc: 'direccion de la ciudad para obtener el clima',
         demand: true
     }
-}).argv;
+}).argv
 
-console.log(argv.direccion);
-console.log(argv.direccion);
+let encodeUrl = encodeURI(argv.direccion);
+
+axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${ encodeUrl }&key=AIzaSyBdrk_BY_noe1tVeV3rNLJYg8QRYk44uCo`)
+    .then(resp => {
+        let location = resp.data.results[0];
+        let coors = location.geometry.location;
+
+        console.log('Dirección: ', location.formatted_address);
+        console.log('lat', coors.lat);
+        console.log('lng', coors.lng);
+
+
+        // console.log(JSON.stringify(resp.data, undefined, 2));
+
+    })
+    .catch(e => console.log('Error!!!!:', e));
