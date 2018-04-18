adb0e38250c1b1b208caf0dc8a84b1d99389ff31
Jose Garcia <josegarciamanez@gmail.com>
2018-04-18 19:21:39 +0200

    app finalizada

--- app.js
+++ app.js
@@ -9,14 +9,29 @@ const argv = require('yargs').options({
     }
 }).argv
 
-lugar.getLugarLatLng(argv.direccion)
-    .then(resp => {
-        console.log(resp)
-    })
-    .catch(e => console.log(e))
+let getInfo = async(direccion) => {
+    try {
+        let coors = await lugar.getLugarLatLng(direccion);
+        let temp = await clima.getClima(coors.lat, coors.lng);
+        return `El clima en ${coors.direccion} es de ${temp} grados Centígrados en estos momentos`
+    } catch (e) {
+        return `No se pudo determinar el clima en ${direccion}`
+    }
 
-clima.getClima(9.9280694, -84.0907246)
-    .then(temp => console.log(temp))
-    .catch(e => console.log(e))
+}
+
+getInfo(argv.direccion)
+    .then(mensaje => console.log(mensaje))
+    .catch(e => console.log(e));
+
+// lugar.getLugarLatLng(argv.direccion)
+//     .then(resp => {
+//         console.log(resp)
+//     })
+//     .catch(e => console.log(e))
+
+// clima.getClima(9.9280694, -84.0907246)
+//     .then(temp => console.log(temp))
+//     .catch(e => console.log(e))
 
 // PETICION http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&units=metric&appid=6583876280f112240d54a34537a6ba4a
